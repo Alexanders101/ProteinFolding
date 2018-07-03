d4f8f96372c262490f0f7db916e5b21c90dd8158
Alexander Shmakov <ashmakov@uci.edu>
2018-07-03 14:12:06 -0700

    Various small changes
    - Remove legal check from next_state_multi due to compatibility
    - energy -> reward
    - Formatting

--- ProteinEnv.py
+++ ProteinEnv.py
@@ -33,34 +33,35 @@ class NPProtein():
         return state
 
     def next_state_multi(self, state, actions):
-        save = self.legal(state)
-        return [self.next_state(state, x) for x in actions if x in save]
+        return [self.next_state(state, x) for x in actions]
+        # save = self.legal(state)
+        # return [self.next_state(state, x) for x in actions if x in save]
 
     def legal(self, state):
-        L = state.shape[1]
+        size = state.shape[1]
         current_index = state[1, 0] - 1
         last_coord = state[2:, current_index]
         possible_moves = last_coord + self.directions
 
-        possible_moves_H = coord_hash(possible_moves, L)
-        visited_H = set(coord_hash(state[2:, :current_index + 1].T, L))
+        possible_moves_H = coord_hash(possible_moves, size)
+        visited_H = set(coord_hash(state[2:, :current_index + 1].T, size))
 
         return {i for i, h in enumerate(possible_moves_H) if h not in visited_H}
 
     def hash(self, state):
-        return xxhash.xxh64(state).intdigest()
+        return xxhash.xxh64(state, seed=0).intdigest()
         #return state.tostring()
 
     def done(self, state):
-        return state[1,-1]>0
+        return state[1, -1] > 0
 
-    def eval_energy(self, state):
+    def reward(self, state):
         aa_string = state[0, ]
         lattice = state[2:, ].T
         num = state[1, 0]
         tot_energy = 0
         for i in range(num):
             for j in range(i + 2, num):
-                if aa_string[i] + aa_string[j] == 2 and np.linalg.norm(lattice[i, ] - lattice[j, ]) <= self.energy_distance:
+                if aa_string[i] + aa_string[j] == 2 and np.linalg.norm(lattice[i] - lattice[j]) <= self.energy_distance:
                     tot_energy -= 1
-        return tot_energy
+        return -tot_energy
