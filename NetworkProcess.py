1255d8be55adf0a0a2c4f9780d047442f9a086c3
Alexander Shmakov <ashmakov@uci.edu>
2018-07-11 19:39:45 -0700

    Remade Network thread as a seperate process for better multithreading

---
+++ NetworkProcess.py
@@ -0,0 +1,81 @@
+from multiprocessing import Process, Queue, Array
+import keras
+import tensorflow as tf
+import ctypes
+import numpy as np
+
+class NetworkProcess(Process):
+    def __init__(self, make_model, state_shape, num_moves, num_states=1, num_workers=1, batch_size=None,
+                 session_config=None):
+        super(NetworkProcess, self).__init__()
+
+        self.make_model = make_model
+        self.session_config = session_config
+
+        self.state_shape = state_shape
+        self.num_moves = num_moves
+
+        self.num_states = num_states
+        self.num_workers = num_workers
+
+        if batch_size is None:
+            batch_size = num_states * num_workers
+        self.batch_size = batch_size
+
+        self.input_queue = Queue(maxsize=num_workers)
+        self.output_queue = [Queue(1) for _ in range(num_workers)]
+
+        self.__input_buffer_base = Array(ctypes.c_int64, int(num_workers * num_states * np.prod(state_shape)), lock=False)
+        self.input_buffer = np.ctypeslib.as_array(self.__input_buffer_base.get_obj())
+        self.input_buffer = self.input_buffer.reshape(num_workers, num_states, *state_shape)
+
+        self.__policy_buffer_base = Array(ctypes.c_float, int(num_workers * num_states * num_moves), lock=False)
+        self.policy_buffer = np.ctypeslib.as_array(self.__policy_buffer_base.get_obj())
+        self.policy_buffer = self.policy_buffer.reshape(num_workers, num_states, num_moves)
+
+        self.__value_buffer_base = Array(ctypes.c_float, int(num_workers * num_states), lock=False)
+        self.value_buffer = np.ctypeslib.as_array(self.__value_buffer_base.get_obj())
+        self.value_buffer = self.value_buffer.reshape(num_workers, num_states, 1)
+
+    def predict(self, idx, states):
+        assert self.is_alive(), "Network has not been started or has already been shutdown."
+        self.input_buffer[idx, :] = states[:]
+        self.input_queue.put_nowait(idx)
+
+        self.output_queue[idx].get()
+        return self.policy_buffer[idx], self.value_buffer[idx]
+
+    def shutdown(self):
+        self.input_queue.put(-1)
+
+    def __initialize_network(self):
+        self.session = tf.Session(config=self.session_config)
+        keras.backend.set_session(self.session)
+
+        self.model = self.make_model()
+
+    def run(self):
+        self.__initialize_network()
+        ids = np.zeros(self.num_workers, dtype=np.uint16)
+        input_buffer = self.input_buffer.reshape(self.num_workers * self.num_states, *self.state_shape)
+
+        while True:
+            ids[0] = self.input_queue.get()
+            if ids[0] >= self.num_workers:
+                break
+
+            size = 1
+            while size < self.num_workers and not self.input_queue.empty():
+                ids[size] = self.input_queue.get()
+                size += 1
+
+            policy, value = self.model.predict(input_buffer, batch_size=self.batch_size)
+            policy = policy.reshape(self.num_workers, self.num_states, 12)
+            value = value.reshape(self.num_workers, self.num_states, 1)
+
+            for i in range(size):
+                idx = ids[i]
+                self.policy_buffer[idx, :, :] = policy[idx, :, :]
+                self.value_buffer[idx, :, :] = value[idx, :, :]
+
+                self.output_queue[idx].put(1)
