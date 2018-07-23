68a69c257b7e1cbfa03bb78510a6c7a1de4a3133
Daniel Stephens <danielstephens@dhcp-x233-085.mobile.uci.edu>
2018-07-23 16:10:26 -0700

    fixed greedy

--- Greedy_loop.py
+++ Greedy_loop.py
@@ -4,40 +4,51 @@ from ProteinEnv import NPProtein
 import ProteinNetworkUtils
 from tensorflow import keras
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-N = 48
+N=48
 model = make_short_network(N)
 model.compile("adam", loss="MSE", loss_weights=[100, 1])
-filename = "where_weights_saved"
+filename = "/Users/danielstephens/weights_saved_Jul23_epoch_good.h5"
 model.load_weights(filename)
 
-NUM_EPOCHS = 100
-NUM_SAMPLES = 100
+NUM_EPOCHS = 1
+NUM_SAMPLES = 10
 start = NPProtein(N, 1)
 for x in range(NUM_EPOCHS):
     count = 0
     y = np.random.randint(20, 30, NUM_SAMPLES)
-    store = np.zeros(((N - 1) * NUM_SAMPLES, 5, 48), dtype=np.int64)
-    policies = np.zeros(((N - 1) * NUM_SAMPLES, 6))
-    values = np.zeros(((N - 1) * NUM_SAMPLES, 1))
+    store = np.zeros(((N-1)*NUM_SAMPLES, 5, 48), dtype=np.int64)
+    policies = np.zeros(((N-1)*NUM_SAMPLES, 6))
+    values = np.zeros(((N-1)*NUM_SAMPLES, 1))
     states = np.zeros((NUM_SAMPLES, 5, 48), dtype=np.int64)
+    energies = np.zeros(NUM_SAMPLES)
     for z in range(NUM_SAMPLES):
         string = np.zeros(N) + 2
         string[:y[z]] = 1
         string = np.random.permutation(string)
-        states[z] = start.new_state(string)
-    for u in range(N - 1):
-        policy, value = model.predict(states)
-        move = np.argmax(policy, axis=1)
-        store[count:count + NUM_SAMPLES] = states
-        policies[count:count + NUM_SAMPLES] = policy
-        values[count:count + NUM_SAMPLES] = value
-        for w in range(NUM_SAMPLES):
-            states[w] = start.next_state(states[w], move[w])
-        count += NUM_SAMPLES
-    filepath = "weights_saved_greedy_Jul23_epoch_{}.h5".format(x)
-    checkpoint = keras.callbacks.ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=False,
-                                                 mode='max')
-    callbacks_list = [checkpoint]
+        state = start.new_state(string)
+        for u in range(N-1):
+            num_moves = list(start.legal(state))
+            mid = np.zeros((len(num_moves), 5, 48))
+            for t in range(len(num_moves)):
+                mid[t] = start.next_state(state, num_moves[t])
+            if not num_moves:
+                count += 1
+                continue
+            policy, value = model.predict(mid)
+            move = np.argmax(value)
+            store[count] = state
+            values[count] = value[move]
+            policies[count, num_moves[move]] = 1
+            state = start.next_state(state, num_moves[move])
+            count += 1
+        energies[z] = start.reward(state)
+    plt.hist(energies)
+    print(np.mean(energies))
+    filepath="weights_saved_greedy_Jul23_epoch_{}.h5".format(x)
+    #checkpoint = keras.callbacks.ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=False, mode='max')
+    #callbacks_list = [checkpoint]
     print("Epoch {}".format(x))
-    model.fit(x=store, y=[policies, values], callbacks=callbacks_list)
+    model.fit(x=store, y=[policies, values])
+    model.save_weights(filepath)
