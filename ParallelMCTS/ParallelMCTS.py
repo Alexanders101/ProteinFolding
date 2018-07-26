1f736803fa98a8da79cfcefc5bfb33bee3bc662c
Alexander Shmakov <ashmakov@uci.edu>
2018-07-25 17:05:10 -0700

    Added multi-game capactiy to playing the games.

--- ParallelMCTS/ParallelMCTS.py
+++ ParallelMCTS/ParallelMCTS.py
@@ -9,7 +9,7 @@ Version Notes
 -------------
 1.1: Adapted AsyncMCTS to work with Protein Environment
 
-0.9: Implemented a version of virutal loss. However, this virtual loss is currently only calculated
+0.9: Implemented a version of virtual loss. However, this virtual loss is currently only calculated
 for each edge, not each node. This means its possible to end up at the same state if you take a different
 path to it. Ideally, for 1.0, implement a full node-based virtual loss.
 
@@ -25,14 +25,14 @@ from ParallelMCTS.SinglePlayerEnvironment import SinglePlayerEnvironment
 import numpy as np
 import tensorflow as tf
 from typing import Callable, Tuple
-from time import time
+from time import time, sleep
 
 
+# noinspection PyPep8Naming
 class ParallelMCTS:
     CONFIG_DEFAULTS = {
         "calculation_time": 15,
         "C": 1.4,
-        "batch_size": None,
         "temperature": 1.0,
         "epsilon": 0.3,
         "alpha": 0.03,
@@ -42,9 +42,11 @@ class ParallelMCTS:
         "backup_true_value": False
     }
 
+    # noinspection PyDefaultArgument
     def __init__(self, env: SinglePlayerEnvironment, make_model: Callable[[], tf.keras.Model],
-                 num_workers: int = 2, num_networks: int = 4, session_config: tf.ConfigProto = None, *,
-                 simulation_manager=SimulationProcessManager, simulation_options: dict = {},
+                 num_games: int = 1, num_workers: int = 2, num_networks: int = 4,
+                 session_config: tf.ConfigProto = None, *,
+                 simulation_manager: type = SimulationProcessManager, simulation_options: dict = {},
                  network_options: dict = {}, database_options: dict = {}, **kwargs):
         """
         Create a Monte Carlo tree search with asynchronous simulation.
@@ -56,6 +58,8 @@ class ParallelMCTS:
         make_model : () -> keras.Model
             A function defining how to create your model.
             The resulting network has the following signature: State -> (Policy, Value)
+        num_games : int
+            Number of maximum parallel games to play.
         num_workers : int
             Number of simulation workers for a single game.
         num_networks : int
@@ -79,8 +83,6 @@ class ParallelMCTS:
             Number of seconds to allow for selecting a move
         C : default = 1.4
             Exploration parameter for the node selection
-        batch_size : default = None
-            Batch size for prediction. Defaults to num_threads * num_moves for optimality.
         temperature : default = 1.0
             Exploration parameter for move selection
         epsilon : default = 0.3
@@ -100,7 +102,13 @@ class ParallelMCTS:
         # Set up environment
         self.env = env
         self.num_moves = len(self.env.moves)
-        self.state_shape = self.env.state_shape
+        self.state_shape = tuple(self.env.state_shape)
+
+        # Class parameters
+        self.num_games = num_games
+        self.num_workers = num_workers
+        self.num_networks = num_networks
+        self.num_total_workers = num_workers * num_games
 
         # Run Dynamic Config setup
         self.set_config(**kwargs)
@@ -108,42 +116,51 @@ class ParallelMCTS:
         # Fix alpha to be an array of alphas
         self.alpha = np.repeat(self.alpha, repeats=self.num_moves)
 
-        # Simulation database
-        self.database = DataProcess(self.num_moves, num_workers, single_tree=self.single_tree, **database_options)
-
         # Setup Networks
         self.network_manager = NetworkManager(make_network=make_model,
                                               state_shape=self.state_shape,
                                               num_moves=self.num_moves,
                                               num_states=1,
-                                              num_workers=num_workers,
-                                              batch_size=self.batch_size,
+                                              num_workers=self.num_total_workers,
                                               num_networks=num_networks,
                                               session_config=session_config,
                                               **network_options)
 
-        # Setup Simulation Workers
-        self.workers = simulation_manager(num_workers, env, self.network_manager, self.database, self.get_config(),
-                                          **simulation_options)
+        # Simulation Workers and Database setup
+        self.databases = []
+        self.workers = []
+        for idx in range(num_games):
+            database = DataProcess(self.num_moves, num_workers, single_tree=self.single_tree, **database_options)
+            worker = simulation_manager(idx, num_workers, env, self.network_manager, database, self.get_config(),
+                                        **simulation_options)
+            self.databases.append(database)
+            self.workers.append(worker)
 
     def start(self, training_network_wait_time: float = 3):
         print("Starting Networks")
         self.network_manager.start(wait_time=training_network_wait_time)
         self.network_manager.wait_until_all_ready()
 
-        print("Starting Database")
-        self.database.start()
+        print("Starting Databases")
+        for database in self.databases:
+            database.start()
 
+        sleep(1)
         print("Starting Workers")
-        self.workers.start()
+        for worker in self.workers:
+            worker.start()
 
     def shutdown(self):
         print("Shutting Down Workers")
-        self.workers.shutdown()
+        for worker in self.workers:
+            worker.shutdown()
+
         print("Shutting Down Network Manager")
         self.network_manager.shutdown()
+
         print("Shutting Down Database")
-        self.database.shutdown()
+        for database in self.databases:
+            database.shutdown()
 
     def __enter__(self):
         self.start()
@@ -151,6 +168,7 @@ class ParallelMCTS:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.shutdown()
 
+    # noinspection PyListCreation
     def __str__(self):
         out = []
         out.append("="*50)
@@ -182,6 +200,7 @@ class ParallelMCTS:
         options['train_buffer_size'] = 64
         options['start_port'] = 2222
         options['num_ps'] = None
+        options['batch_size'] = None
         return options
 
     @staticmethod
@@ -215,9 +234,15 @@ class ParallelMCTS:
             out[name] = self.__getattribute__(name)
         return out
 
-    def clear(self) -> None:
-        """ Clear all of the simulation memory. """
-        self.database.clear()
+    def clear(self, idx: int) -> None:
+        """ Clear all of the simulation memory.
+
+        Parameters
+        ----------
+        idx : int
+            Which simulation worker's database to clear.
+        """
+        self.databases[idx].clear()
 
     def fit_epoch(self, num_games: int = 1) -> None:
         """ Play a certain number of games and then train on the resulting data.
@@ -245,16 +270,17 @@ class ParallelMCTS:
 
         self.network_manager.fit(states, policies, values)
 
-    def play(self, start_state: np.ndarray, clear: bool = True) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    def play(self, worker_idx: int, start_state: np.ndarray, clear: bool = True) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
         Play a full episode and return training data.
 
         Parameters
         ----------
+        worker_idx : int
+            Index of this simulation worker.
         start_state : np.ndarray
             Starting state for the episode.
-
-        clear : bool
+        clear : bool = True
             Whether or not to clear MCTS databases before playing. This should be TRUE
             during training and FALSE during playing.
 
@@ -273,27 +299,29 @@ class ParallelMCTS:
         values where it had information, T can be anywhere from [1, N], with N being the maximum length of the protein.
         """
         if clear:
-            self.clear()
+            self.clear(worker_idx)
 
-        state = start_state  # Current state of the game, this will be updated during play
+        # Current state of the game, this will be updated during play
+        state = start_state.copy()
 
-        # #####
         # Return Buffers
-        # ################
         states = np.zeros(shape=(self.env.max_length - 1, *self.env.state_shape), dtype=np.int64)
         pis = np.zeros(shape=(self.env.max_length - 1, self.env.moves.shape[0]), dtype=np.float32)
 
-        # #####
         # Play game until finished
-        # ###########################
         t = 0
         while not self.env.done(state):
-            pi = self.select_moves(state)
+            legal_moves = self.env.legal(state)
+            if len(legal_moves) == 0:
+                break
+
+            pi = self.select_moves(worker_idx, state)
 
             states[t, :] = state
             pis[t, :] = pi
             t += 1
 
+            # Todo Limit to legal moves
             next_move = np.random.choice(np.arange(0, len(pi)), p=pi)
             state = self.env.next_state(state, next_move)
 
@@ -304,12 +332,76 @@ class ParallelMCTS:
         # Training Data
         return states[:t], pis[:t], R
 
-    def select_moves(self, state: np.ndarray) -> np.ndarray:
+    def _temperature_policy(self, N: np.ndarray) -> np.ndarray:
+        """ Calculate Alpha-Go Zero policy from visit counts.
+
+        Parameters
+        ----------
+        N : np.ndarray
+            Visit counts of root node.
+
+        Returns
+        -------
+        PI : np.ndarray
+            A distribution over possible moves determining which one will be optimal from the given state.
+        """
+        t = 1 / self.temperature
+        pi = N ** t + np.finfo(np.float32).eps  # Add tiny value to remove 0s
+        pi = pi / pi.sum()
+        return pi
+
+    def select_moves_async(self, worker_idx: int, state: np.ndarray) -> None:
         """
         Calculate a policy from a starting state using MCTS.
 
         Parameters
         ----------
+        worker_idx : int
+            Index of this simulation worker.
+        state : ndarray
+            Starting state.
+        """
+        worker = self.workers[worker_idx]
+
+        # Start simulation on new state
+        worker.set_start_state(state)
+        worker.simulation(clear_tree=True)
+
+    def select_moves_result(self, worker_idx: int) -> np.ndarray:
+        """ Get result of an async select_moves call.
+
+        Behaviour is not defined if select_moves_async was not called prior.
+
+        Parameters
+        ----------
+        worker_idx : int
+            Index of this simulation worker.
+
+        Returns
+        -------
+        PI : np.ndarray
+            A distribution over possible moves determining which one will be optimal from the given state.
+        """
+        worker = self.workers[worker_idx]
+        database = self.databases[worker_idx]
+        state = worker.starting_state.copy()
+
+        # Block and wait for results
+        results = worker.results()
+
+        if self.verbose >= 2:
+            print("{} Nodes per second".format(np.mean(results['nodes_per_second'])))
+
+        return self._temperature_policy(database.get(0, self.env.hash(state))[0])
+
+    def select_moves(self, worker_idx: int, state: np.ndarray) -> np.ndarray:
+        """
+        Calculate a policy from a starting state using MCTS.
+
+        Parameters
+        ----------
+        worker_idx : int
+            Index of this simulation worker.
         state : ndarray
             Starting state.
 
@@ -319,26 +411,17 @@ class ParallelMCTS:
             A distribution over possible moves determining which one will be optimal from the given state.
 
         """
-        episode_start_time = time()
+        worker = self.workers[worker_idx]
+        database = self.databases[worker_idx]
 
         # Start simulation on new state
-        self.workers.set_start_state(state)
-        self.workers.simulation(clear_tree=True)
+        worker.set_start_state(state)
+        worker.simulation(clear_tree=True)
 
         # Block and wait for results
-        results = self.workers.results()
+        results = worker.results()
 
         if self.verbose >= 2:
-            print("{} Nodes per second".format(np.sum(results['num_nodes']) / (time() - episode_start_time)))
-            print("Avg RunTime: {}".format(np.mean(results['mean_run_time'])))
-            print("Avg PredTime: {}".format(np.mean(results['mean_pred_time'])))
+            print("{} Nodes per second".format(np.mean(results['nodes_per_second'])))
 
-        # #####
-        # Calculate policy distribution
-        # ######################
-        t = 1 / self.temperature
-        pi = self.database.get(0, self.env.hash(state))[0]
-        pi = pi ** t + np.finfo(np.float32).eps  # Add tiny value to remove 0s
-        pi = pi / pi.sum()
-
-        return pi
+        return self._temperature_policy(database.get(0, self.env.hash(state))[0])
